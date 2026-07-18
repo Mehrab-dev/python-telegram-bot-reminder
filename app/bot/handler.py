624402d3959ef7b2f7b30c07f4daa205210dc6c7
Mehrab <programmer.py.mail@gmail.com>
2026-07-18 16:10:33 +0330

    completing back button, get API Key from Groq and install groq modules and

--- app/bot/handler.py
+++ app/bot/handler.py
@@ -8,10 +8,16 @@ from app.database.models import TaskModel
 
 user_state = {}
 user_temp_task = {}
+user_history = {}
 
 
 @bot.message_handler(commands=["start"])
 def start_handler(message):
+    user_id = message.from_user.id
+
+    if user_id not in user_history:
+        user_history[user_id] = []
+
     user_id = message.chat.id
     user_state[user_id] = "main_menu"
     first_name = message.chat.first_name
@@ -28,6 +34,11 @@ def start_handler(message):
 @bot.callback_query_handler(func=lambda call: call.data == "add_task")
 def add_task_handler(call):
     user_id = call.from_user.id
+
+    if user_id not in user_history:
+        user_history[user_id] = []
+    user_history[user_id].append(user_state[user_id])
+
     user_state[user_id] = "waiting_title"
     
     bot.edit_message_text(
@@ -42,6 +53,12 @@ def add_task_handler(call):
 @bot.callback_query_handler(func=lambda call: call.data == "list_task")
 def list_task_handler(call):
     user_id = call.from_user.id
+
+    if user_id not in user_history:
+        user_history[user_id] = []
+    user_history[user_id].append(user_state[user_id])
+
+    user_state[user_id] = "waiting_task_list"
 
     db = LocalSession()
     tasks = db.query(TaskModel).filter_by(user_id=user_id).all()
@@ -70,6 +87,12 @@ def get_detail_task(call):
     task_id = int(call.data.split("_")[1])
     user_id = call.from_user.id
 
+    if user_id not in user_history:
+        user_history[user_id] = []
+    user_history[user_id].append(user_state[user_id])
+
+    user_state[user_id] = "waiting_task_detail"
+
     db = LocalSession()
     task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
     db.close()
@@ -95,6 +118,11 @@ def update_title_handler(call):
     user_temp_task[user_id] = {
         "task_id" : task_id
     }
+
+    if user_id not in user_history:
+        user_history[user_id] = []
+    user_history[user_id].append(user_state[user_id])
+
     user_state[user_id] = "waiting_new_title"
 
     keyboard = InlineKeyboardMarkup()
@@ -117,6 +145,11 @@ def update_description_handler(call):
     user_temp_task[user_id] = {
         "task_id" : task_id
     }
+
+    if user_id not in user_history:
+        user_history[user_id]= []
+    user_history[user_id].append(user_state[user_id])
+
     user_state[user_id] = "waiting_new_des"
 
     keyboard = InlineKeyboardMarkup()
@@ -139,6 +172,11 @@ def update_due_date_handler(call):
     user_temp_task[user_id] = {
         "task_id" : task_id
     } 
+
+    if user_id not in user_history:
+        user_history[user_id] = []
+    user_history[user_id].append(user_state[user_id])
+
     user_state[user_id] = "waiting_new_due"
 
     keyboard = InlineKeyboardMarkup()
@@ -153,7 +191,180 @@ def update_due_date_handler(call):
     )
 
 
+# back handler
+@bot.callback_query_handler(func=lambda call: call.data == "back")
+def back_handler(call):
+    user_id = call.from_user.id
+    if user_id not in user_history:
+        user_history[user_id] = []
+    state = user_state[user_id]
 
+    if state == "waiting_title":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+
+        first_name = call.message.chat.first_name
+
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=f"سلام {first_name} عزیز 👋" \
+            "لطفا یکی از گزینه های زیر را انتخاب کن",
+            reply_markup=start_keyboard()
+        )
+
+
+    elif state == "waiting_description":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+
+        bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text="🔹 نام وظیفه خود را وارد کنید :",
+        reply_markup=add_title_task_keyboard()
+    )
+        
+    
+    elif state == "waiting_due_date":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text="✏️ توضیحات وظیفه خود را وارد کنید :",
+            reply_markup=add_des_task_keyboard()
+        )
+    
+
+    elif state == "waiting_task_list":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+        first_name = call.message.chat.first_name
+
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=f"سلام {first_name} عزیز 👋" \
+            "لطفا یکی از گزینه های زیر را انتخاب کن",
+            reply_markup=start_keyboard()
+        )
+    
+
+    elif state == "waiting_task_detail":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+
+        db = LocalSession()
+        tasks = db.query(TaskModel).filter_by(user_id=user_id).all()
+        keyboard = InlineKeyboardMarkup()
+        for item in tasks:
+            task_button = InlineKeyboardButton(
+                text=item.title,
+                callback_data=f"task_{item.id}"
+            )
+            keyboard.add(task_button)
+        
+        back_btn = InlineKeyboardButton(text="🔙 بازگشت", callback_data="back")
+        keyboard.add(back_btn)
+
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text="📋 لیست وظایف :",
+            reply_markup=keyboard
+        )
+    
+
+    elif state == "waiting_new_title":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+        task_id = user_temp_task[user_id]["task_id"]
+
+        db = LocalSession()
+        task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
+        db.close()
+
+        response = f"""
+                🔹 نام وظیفه : {task.title}
+                📂 توضیحات وظیفه : {task.description}
+                📆 تاریخ اعلان : {task.due_date}
+            """
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=response,
+            reply_markup=task_detail_keyboard(task_id)
+        )
+    
+
+    elif state == "waiting_new_des":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+        task_id = user_temp_task[user_id]["task_id"]
+
+        db = LocalSession()
+        task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
+        db.close()
+
+        response = f"""
+                🔹 نام وظیفه : {task.title}
+                📂 توضیحات وظیفه : {task.description}
+                📆 تاریخ اعلان : {task.due_date}
+            """
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=response,
+            reply_markup=task_detail_keyboard(task_id)
+        )
+
+
+    elif state == "waiting_new_due":
+        previous_state = user_history[user_id].pop()
+        user_state[user_id] = previous_state
+        task_id = user_temp_task[user_id]["task_id"]
+
+        db = LocalSession()
+        task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
+        db.close()
+
+        response = f"""
+                🔹 نام وظیفه : {task.title}
+                📂 توضیحات وظیفه : {task.description}
+                📆 تاریخ اعلان : {task.due_date}
+            """
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=response,
+            reply_markup=task_detail_keyboard(task_id)
+        )
+
+
+
+# back home handler
+@bot.callback_query_handler(func=lambda call: call.data == "back_home")
+def back_home_handler(call):
+    user_id = call.from_user.id
+    first_name = call.message.chat.first_name
+
+    user_state[user_id] = "main_menu"
+    
+    if user_id in user_temp_task:
+        del user_temp_task[user_id]
+    
+    if user_id in user_history:
+        user_history[user_id] = []
+
+        bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id,
+        text=f"سلام {first_name} عزیز 👋" \
+        "لطفا یکی از گزینه های زیر را انتخاب کن",
+        reply_markup=start_keyboard()
+    )
 
 
 # text handlers
@@ -166,6 +377,11 @@ def text_handler(message):
         user_temp_task[user_id] = {
             "title" : message.text
         }
+
+        if user_id not in user_history:
+            user_history[user_id] = []
+        user_history[user_id].append(user_state[user_id])
+
         user_state[user_id] = "waiting_description"
         bot.send_message(
             chat_id=message.chat.id,
@@ -176,6 +392,11 @@ def text_handler(message):
     
     elif state == "waiting_description":
         user_temp_task[user_id]["description"] = message.text
+
+        if user_id not in user_history:
+            user_history[user_id] = []
+        user_history[user_id].append(user_state[user_id])
+
         user_state[user_id] = "waiting_due_date"
 
         bot.send_message(
@@ -212,6 +433,11 @@ def text_handler(message):
     
     elif state == "waiting_new_title":
         task_id = user_temp_task[user_id]["task_id"]
+
+        if user_id not in user_history:
+            user_history[user_id] = []
+        user_history[user_id].append(user_state[user_id])
+
         db = LocalSession()
         task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
         task.title = message.text
@@ -231,6 +457,10 @@ def text_handler(message):
     
     elif state == "waiting_new_des":
         task_id = user_temp_task[user_id]["task_id"]
+        
+        if user_id not in user_history:
+            user_history[user_id] = []
+        user_history[user_id].append(user_state[user_id])
 
         db = LocalSession()
         task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
@@ -251,6 +481,10 @@ def text_handler(message):
     
     elif state == "waiting_new_due":
         task_id = user_temp_task[user_id]["task_id"]
+        
+        if user_id not in user_history:
+            user_history[user_id] = []
+        user_history[user_id].append(user_state[user_id])
 
         db = LocalSession()
         task = db.query(TaskModel).filter_by(id=task_id, user_id=user_id).one()
